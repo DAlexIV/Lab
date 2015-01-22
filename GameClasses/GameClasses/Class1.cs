b9f95fb6a944b37c4a0dfe817e9cdd319d832fa2
DAlexIV <dalexiv@yandex.ru>
2015-01-22 16:15:56 +0300

    Initial commit

---
+++ GameClasses/GameClasses/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClasses
+{
+    static char = '#';
+    public class Player
+    {
+        int x;
+        int y;
+        char m;
+        string name;
+    }
+    public class Map
+    {
+        int[,] map = new int[30, 60];
+        // Игроки имеют номера с -1 до -16 в карте
+        char[] players = new char[16];
+    }
+}
---
+++ Serv/Serv/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static int listenPort = 11000;
+    static IPEndPoint ep;
+    static bool is_input = false;
+    static Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    static UdpClient listener = new UdpClient(listenPort);
+    static void Thre()
+    {
+        while (true)
+        {
+            ep = new IPEndPoint(Listener().Address, 11000);
+            is_input = true;
+        }
+    }
+    static IPEndPoint Listener()
+    { 
+
+        
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+        IPEndPoint tmp = new IPEndPoint(IPAddress.Any, IPEndPoint.MaxPort);
+        Console.WriteLine("Waiting for broadcast");
+        byte[] bytes = listener.Receive(ref groupEP);
+        Console.WriteLine("Received broadcast from {0} :\n {1}\n",
+        groupEP.ToString(),
+        Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+        Console.WriteLine(groupEP.Address);
+        return groupEP;
+    }
+    static void Main(string[] args)
+    {
+        
+        string line = Console.ReadLine();
+        byte[] sendbuf = Encoding.ASCII.GetBytes(line);
+
+        Thread Ls = new Thread(Thre);
+        Ls.Start();
+        while (true)
+        {
+            if (is_input)
+            {
+                s.SendTo(sendbuf, ep);
+                is_input = false;
+            }
+            else
+                Thread.Sleep(100);
+        }
+    }
+}
