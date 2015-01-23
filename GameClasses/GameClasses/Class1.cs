e336081e24ab6e5eb8cea49f5c3e9dc18f1f6c8d
DAlexIV <dalexiv@yandex.ru>
2015-01-23 12:14:18 +0300

    Added notification line into map

--- GameClasses/GameClasses/Class1.cs
+++ GameClasses/GameClasses/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameClasses
 {
-    static char = '#';
+    
     public class Player
     {
         int x;
@@ -16,8 +16,16 @@ namespace GameClasses
     }
     public class Map
     {
+        static char defch = '#';
         int[,] map = new int[30, 60];
+
         // Игроки имеют номера с -1 до -16 в карте
         char[] players = new char[16];
+        string[] players = new string[16]; //Никнеймы игроков(чтобы показываться в где-нибудь)
+
+        //Строка для уведомлений игроков, тоже должна показываться где-нибудь в течении какого-нибудь времени
+        //В идеале это может быть даже некоторый набор строк, то есть будут показываться последние несколько
+        //В случае отсутствия каких-либо действий равна null
+        string notif; 
     }
 }
